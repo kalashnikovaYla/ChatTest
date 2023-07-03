d8422427b6b9f57ab3601df60ebcc6bea70b5c47
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-07-03 22:59:06 +0500

    Initial Commit

---
+++ ChatTest/ChatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTestApp.swift
+//  ChatTest
+//
+//  Created by sss on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
