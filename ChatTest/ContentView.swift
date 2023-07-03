7e6238780917774e9105d363a1ff8934a4084b0f
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-07-03 23:15:21 +0500

    Initial

--- ChatTest/ContentView.swift
+++ ChatTest/ContentView.swift
@@ -8,19 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageText = ""
+    @State var messages: [String] = ["Welcome to Chat Bot 2.0!"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("iBot")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Image(systemName: "bubble.left.fill")
+                    .font(.system(size: 26))
+                    .foregroundColor(Color.blue)
+            }
+            
+            ScrollView {
+                ForEach(messages, id: \.self) { message in
+                    // If the message contains [USER], that means it's us
+                    if message.contains("[USER]") {
+                        let newMessage = message.replacingOccurrences(of: "[USER]", with: "")
+                        
+                        // User message styles
+                        HStack {
+                            Spacer()
+                            Text(newMessage)
+                                .padding()
+                                .foregroundColor(Color.white)
+                                .background(Color.blue.opacity(0.8))
+                                .cornerRadius(10)
+                                .padding(.horizontal, 16)
+                                .padding(.bottom, 10)
+                        }
+                    } else {
+                        
+                        // Bot message styles
+                        HStack {
+                            Text(message)
+                                .padding()
+                                .background(Color.gray.opacity(0.15))
+                                .cornerRadius(10)
+                                .padding(.horizontal, 16)
+                                .padding(.bottom, 10)
+                            Spacer()
+                        }
+                    }
+                    
+                }.rotationEffect(.degrees(180))
+            }
+            .rotationEffect(.degrees(180))
+            .background(Color.gray.opacity(0.1))
+            
+            
+            // Contains the Message bar
+            HStack {
+                TextField("Type something", text: $messageText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .onSubmit {
+                        sendMessage(message: messageText)
+                    }
+                
+                Button {
+                    sendMessage(message: messageText)
+                } label: {
+                    Image(systemName: "paperplane.fill")
+                }
+                .font(.system(size: 26))
+                .padding(.horizontal, 10)
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func sendMessage(message: String) {
+        withAnimation {
+            messages.append("[USER]" + message)
+            self.messageText = ""
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation {
+                    messages.append(getBotResponse(message: message))
+                }
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+func getBotResponse(message: String) -> String {
+    let tempMessage = message.lowercased()
+    
+    if tempMessage.contains("hello") {
+        return "Hey there!"
+    } else if tempMessage.contains("goodbye") {
+        return "Talk to you later!"
+    } else if tempMessage.contains("how are you") {
+        return "I'm fine, how about you?"
+    } else {
+        return "That's cool."
     }
 }
